2657ba7ce1f30b67393bd032742b3d98fe12fd17
TGS Berin <berinmadzak@gmail.com>
2023-06-05 18:58:36 +0200

    Added components

---
+++ src/components/Chatroom.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Chatroom() {
+    return (
+        <div>
+            <form>
+                <input type="text" placeholder='Enter text...'/>
+                <input type="submit" />
+            </form>
+        </div>
+    )
+}
